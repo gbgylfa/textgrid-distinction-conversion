06f1412b8b98888fadacc0a0ba850b3ffed9637e
gb.gylfa <gb.gylfa@gmail.com>
2018-02-01 11:25:26 -0800

    creates outdirectory if not existing

--- textgrid_distinction_conversion.py
+++ textgrid_distinction_conversion.py
@@ -22,22 +22,22 @@ import numpy as np
 def replaceThetas(phoneTier, wordTier):
     '''Loops through words in a tier, finds words that have ce, ci, or z in them, finds all letters that will correspond to an S in Latin American transcription, gets positions of the ones that should be corrected to Z, replaces the corresponding S phones on the phone tier.'''
     for interval in wordTier:
-    	word = interval.text
-    	if containsTheta(word):
-        	instances = re.findall('X|S|Z|CE|CI|C\xc9|C\xcd', word)
-        	to_replace = np.where([x in ['Z','CE','CI','C\xc9','C\xcd'] for x in instances])[0]
-        	count = -1
-        	phones = phoneTier.tslice(interval.t1, interval.t2, lstrip=True, rstrip=True)
-        	phoneList = [phone.text for phone in phones]
-        	if phoneList.count('s') != len(instances):
-        		print("Unexpected number of S in " + word)
-        	else:
-        		for idx, phone in enumerate(phones):
-        			if phone.text == 's':
-        				count += 1
-        				if count in to_replace:
-        					phone.text = 'z'
-        					continue
+        word = interval.text
+        if containsTheta(word):
+            instances = re.findall('X|S|Z|CE|CI|C\xc9|C\xcd', word)
+            to_replace = np.where([x in ['Z','CE','CI','C\xc9','C\xcd'] for x in instances])[0]
+            count = -1
+            phones = phoneTier.tslice(interval.t1, interval.t2, lstrip=True, rstrip=True)
+            phoneList = [phone.text for phone in phones]
+            if phoneList.count('s') != len(instances):
+                print("Unexpected number of S in " + word)
+            else:
+                for idx, phone in enumerate(phones):
+                    if phone.text == 's':
+                        count += 1
+                        if count in to_replace:
+                            phone.text = 'z'
+                            continue
 
 
 def containsTheta(word):
@@ -45,29 +45,34 @@ def containsTheta(word):
 
 
 def getTextGridFiles(directory):
-	'''Returns full path of all .TextGrid files in the directory'''
-	tgs = glob.glob(os.path.join(directory,"*.TextGrid"))
-	return tgs
+    '''Returns full path of all .TextGrid files in the directory'''
+    tgs = glob.glob(os.path.join(directory,"*.TextGrid"))
+    return tgs
 
 
 
 def newTgFromLabelManager(lm, new_name, directory):
-	'''takes a label manager object, a new file basename, and an output directory and calls as_string to create a new TextGrid file'''
-	outfile = os.path.join(directory, new_name)
-	with codecs.open(outfile,'w','utf-8') as o:
-	    o.write(lm.as_string('praat_short'))
+    '''takes a label manager object, a new file basename, and an output directory and calls as_string to create a new TextGrid file'''
+    outfile = os.path.join(directory, new_name)
+    with codecs.open(outfile,'w','utf-8') as o:
+        o.write(lm.as_string('praat_short'))
 
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 if __name__ == "__main__":
-	indirectory = sys.argv[1]
-	outdirectory = sys.argv[2]
-	tgs = getTextGridFiles(indirectory)
-	for tg in tgs:
-		current_name = os.path.basename(tg)
-		new_name = os.path.splitext(current_name)[0] + "_z" + ".TextGrid"
-		lm = LabelManager(tg, 'praat', codec = 'utf-8')
-		for ix in range(0,len(lm.names),2):
-			replaceThetas(lm.tier(ix), lm.tier(ix+1))
-		newTgFromLabelManager(lm, new_name, outdirectory)
+    indirectory = sys.argv[1]
+    outdirectory = sys.argv[2]
+    tgs = getTextGridFiles(indirectory)
+    for tg in tgs:
+        current_name = os.path.basename(tg)
+        new_name = os.path.splitext(current_name)[0] + "_z" + ".TextGrid"
+        lm = LabelManager(tg, 'praat', codec = 'utf-8')
+        for ix in range(0,len(lm.names),2):
+            replaceThetas(lm.tier(ix), lm.tier(ix+1))
+        ensure_dir(outdirectory)
+        newTgFromLabelManager(lm, new_name, outdirectory)
 
-		
+        
